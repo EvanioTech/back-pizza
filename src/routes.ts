3af39d5b2085ab1a1390b7f86a63e2b269b02409
Evanio Tech <evanioshark@gmail.com>
2025-01-04 23:16:59 -0300

    rota de listar categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
-
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 // ROTAS USER
@@ -18,6 +18,8 @@ router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 // ROTAS CATEGORY
 router.post("/category", isAuthenticated, new CreateCategoryController().handle);
+
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 
 
