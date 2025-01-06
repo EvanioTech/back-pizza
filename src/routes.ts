9f6e2bf42f03da4cc2bbe4e2173230f0b6ed1e50
Evanio Tech <evanioshark@gmail.com>
2025-01-05 23:46:22 -0300

    criando rota de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 // ROTAS USER
@@ -20,6 +21,10 @@ router.get("/me", isAuthenticated, new DetailUserController().handle);
 router.post("/category", isAuthenticated, new CreateCategoryController().handle);
 
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// ROTAS PRODUCTS
+
+router.post("/product", isAuthenticated, new CreateProductController().handle);
 
 
 
